5b359ffa99c53be7ce93d0a8761510bf70e153f2
D2JS <d2js@TECHIN.INTERNAL>
2024-11-05 08:39:55 +0400

    1233232

--- tinklapiu-kurimas/13diena/4/script.js
+++ tinklapiu-kurimas/13diena/4/script.js
@@ -2,4 +2,4 @@ const pirma = +prompt("Koks automobilio greitis km/h ?");
 console.log(pirma);
 
 const suma = 264/(pirma*1000/3600);
-console.log("Automobilis tunelį parvažious per",suma,"s");
+console.log("Automobilis tunelį parvažious per",suma.toFixed(2),"s");
